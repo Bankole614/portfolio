307812b3126f0397c5892a07c1b31a0d727673a4
Bankole614 <bankoleayobami01@gmail.com>
2025-12-09 18:06:33 +0100

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -86,7 +86,7 @@ const Header = () => {
         </nav>
       </header>
       <Dialog open={isMenuOpen} onOpenChange={setIsMenuOpen}>
-        <DialogContent className="max-w-md bg-background/50 backdrop-blur rounded-2xl border border-neutral-700">
+        <DialogContent className="max-w-md bg-background/50 backdrop-blur rounded-2xl border border-border/70">
           <DialogHeader>
             <DialogTitle className='text-start text-xl'>{t('nav.menuTitle')}</DialogTitle>
           </DialogHeader>
